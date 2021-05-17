1778f0e9008e3b6ceb0d21c5278f22f2e5316ac3
StefanRucsandra <rucsiismile@yahoo.com>
2021-05-18 00:21:14 +0300

    artist main nearly done

---
+++ src/main/java/controllers/art_addController.java
@@ -0,0 +1,33 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+
+public class art_addController {
+
+    @FXML
+    private BorderPane DescriptionPane;
+
+    @FXML
+    private Button addart;
+
+    @FXML
+    private TextField adresstextfield;
+
+    @FXML
+    private TextField pricetextfield;
+
+    @FXML
+    private TextField utilitiestextfield;
+
+    @FXML
+    private TextField descriptiontextfield;
+
+    @FXML
+    void addartAction() {
+
+    }
+
+}
