9fc58f2a4dc230c4d4df8a1fe1d508c4f8638729
ioanaivaschescu <ioanaiva@yahoo.com>
2021-05-17 22:19:42 +0300

    LOGIN FXML IMPLEMENTED

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,39 @@
-public class Main {
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+
+
+public class Main extends Application {
+
+
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+//        UserService.loadUsersFromFile();
+//
+//        ApartmentService.loadAFromFile();
+//
+//        OffersService.loadOffersFromFile();
+
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("login.fxml"));
+        primaryStage.setScene(new Scene(root, 737, 524));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+
+    }
+
+
 }
